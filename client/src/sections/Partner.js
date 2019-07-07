ce573f91bbd52ff4e4b94fe996b930fe16606ca4
Hyegyeong310 <hyegyeong310@gmail.com>
2019-07-08 01:31:08 +0900

    add partners and blog-section

--- client/src/sections/Partner.js
+++ client/src/sections/Partner.js
@@ -11,9 +11,24 @@ class Partner extends Component {
       item: { id, title, subtitle }
     } = this.props;
     return (
-      <div className="section">
+      <div className="section partner">
         <div className="section-content" id={`section${id}`}>
           <SectionTitle title={title} subtitle={subtitle} />
+        </div>
+        <div className="blog-content">
+          <div className="section-content">
+            <p>블로그에서 최신 정보를 확인하세요.</p>
+            <p>
+              <a
+                className="blog-btn"
+                href="http://monolabs.io/blog"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                BLOG
+              </a>
+            </p>
+          </div>
         </div>
       </div>
     );
