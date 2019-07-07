d01909c1b90eae67b4f6ff45b970610a13b6bc51
Hyegyeong310 <hyegyeong310@gmail.com>
2019-07-08 03:55:42 +0900

    add problem style and animation

--- client/src/sections/Problem.js
+++ client/src/sections/Problem.js
@@ -4,20 +4,50 @@ import SectionTitle from '../component/SectionTitle';
 class Problem extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      active: false
+    };
   }
+
+  componentDidMount() {
+    window.addEventListener('scroll', event => {
+      let problemTop = document
+        .querySelector('#section2')
+        .getBoundingClientRect().top;
+      if (window.scrollY >= problemTop) {
+        this.setState({ active: true });
+      } else {
+        this.setState({ active: false });
+      }
+    });
+  }
+
   render() {
     const {
-      item: { id, title, subtitle }
+      item: { id, title, subtitle, content }
     } = this.props;
+    const { active } = this.state;
     return (
-      <div className="section">
+      <div className="section problem">
         <div className="section-content" id={`section${id}`}>
           <SectionTitle title={title} subtitle={subtitle} />
+          <div className="card-content">
+            {content.map((text, index) => (
+              <Card key={index} text={text} active={active} />
+            ))}
+          </div>
         </div>
       </div>
     );
   }
+}
+
+function Card({ text, active }) {
+  return (
+    <div className={`card ${active ? 'fadeIn' : ''}`}>
+      <p>{text}</p>
+    </div>
+  );
 }
 
 export default Problem;
