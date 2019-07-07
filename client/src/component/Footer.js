5f127d896684048ca2842efee2ffdd609129ec92
Hyegyeong310 <hyegyeong310@gmail.com>
2019-07-08 00:30:35 +0900

    add footer style and scroll top button

--- client/src/component/Footer.js
+++ client/src/component/Footer.js
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import topBtnImage from '../images/menu_bottom_top_bg.png';
 
-const Footer = () => (
-  <div className="footer">
-    <ul className="icons">
-      <li>
-        <a href="#none" className="icon">
-          <i className="fab fa-facebook-f" />
-        </a>
-      </li>
-      <li>
-        <a href="#none" className="icon">
-          <i className="fab fa-twitter" />
-        </a>
-      </li>
-    </ul>
-    &copy; mono labs corp. {new Date().getFullYear()}
-  </div>
-);
+class Footer extends Component {
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  render() {
+    return (
+      <div className="footer">
+        <div id="menu-top" onClick={this.scrollToTop}>
+          <div
+            id="menu-top_inside"
+            style={{ backgroundImage: `url(${topBtnImage})` }}
+          />
+        </div>
+        <ul className="icons">
+          <li>
+            <a
+              href="https://fb.me/monolabs.io"
+              className="icon"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-facebook-f" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://twitter.com/monolabs1"
+              className="icon"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-twitter" />
+            </a>
+          </li>
+        </ul>
+        <p>&copy; mono labs corp. {new Date().getFullYear()}</p>
+      </div>
+    );
+  }
+}
 
 export default Footer;
