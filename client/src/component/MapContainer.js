40cc9c03a844205f269792f0303c2cf5e41e7c36
Hyegyeong310 <hyegyeong310@gmail.com>
2019-07-08 01:10:20 +0900

    add contact us section, map

---
+++ client/src/component/MapContainer.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+
+class MapContainer extends Component {
+  render() {
+    const mapStyles = {
+      width: '100%',
+      height: '100%'
+    };
+    return (
+      <Map
+        google={this.props.google}
+        zoom={15}
+        style={mapStyles}
+        initialCenter={{ lat: 37.498965, lng: 127.0351 }}
+      >
+        <Marker position={{ lat: 37.498965, lng: 127.0351 }} />
+      </Map>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: process.env.REACT_APP_MAP_API
+})(MapContainer);
--- client/src/sections/Contact.js
+++ client/src/sections/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SectionTitle from '../component/SectionTitle';
+import MapContainer from '../component/MapContainer';
 
 class Contact extends Component {
   constructor(props) {
@@ -11,9 +12,24 @@ class Contact extends Component {
       item: { id, title, subtitle }
     } = this.props;
     return (
-      <div className="section">
+      <div className="section contact">
         <div className="section-content" id={`section${id}`}>
           <SectionTitle title={title} subtitle={subtitle} />
+          <ul className="icons">
+            <li>
+              <i className="far fa-envelope fa-2x" />
+              <h5 className="company-mail">contact@monolabs.io</h5>
+            </li>
+            <li>
+              <i className="fas fa-map-marker-alt fa-2x" />
+              <h5 className="company-address">
+                서울 강남구 테헤란로 26길 14 06236
+              </h5>
+            </li>
+          </ul>
+          <div className="map-container">
+            <MapContainer />
+          </div>
         </div>
       </div>
     );
