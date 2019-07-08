80e071f0ac83ae138a275fb5599fab4fe312615b
Hyegyeong310 <hyegyeong310@gmail.com>
2019-07-09 04:15:52 +0900

    change http -> https

--- server/init.js
+++ server/init.js
@@ -1,4 +1,7 @@
 import dotenv from 'dotenv';
+import fs from 'fs';
+import https from 'https';
+
 import app from './app';
 
 dotenv.config();
@@ -8,4 +11,12 @@ const PORT = process.env.PORT || 5000;
 const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
 
-app.listen(PORT, handleListening);
+https
+  .createServer(
+    {
+      key: fs.readFileSync('server.key'),
+      cert: fs.readFileSync('server.cert')
+    },
+    app
+  )
+  .listen(PORT, handleListening);
