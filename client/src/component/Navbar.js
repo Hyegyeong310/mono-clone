ecb2e9d6d16dc0a8939c1cdad6683a975f12a35c
Hyegyeong310 <hyegyeong310@gmail.com>
2019-07-10 02:18:02 +0900

    add mobile hambuger menu event

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -1,49 +1,81 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-scroll';
 
-function Navbar({ data }) {
-  return (
-    <div className="nav" id="navbar">
-      <div className="nav-content">
-        <a href="/">
-          <img
-            src={`${
-              process.env.REACT_APP_AWS_S3
-            }images/mono-logo-horizontal.png`}
-            className="nav-logo"
-            alt="logo"
-          />
-        </a>
-        <ul className="nav-items">
-          {data.map(({ title }, index) => (
-            <Menu key={index} title={title} index={index} />
-          ))}
-        </ul>
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isOpen: false };
+  }
+
+  toggleMenu() {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
+  handleLinkClick() {
+    this.setState({ isOpen: false });
+  }
+
+  render() {
+    const { data } = this.props;
+    const { isOpen } = this.state;
+    return (
+      <div className="nav" id="navbar">
+        <div className="nav-content">
+          <div className="logo">
+            <a href="/">
+              <img
+                src={`${
+                  process.env.REACT_APP_AWS_S3
+                }images/mono-logo-horizontal.png`}
+                className="nav-logo"
+                alt="logo"
+              />
+            </a>
+            <div id="mobile-btn" onClick={() => this.toggleMenu()}>
+              <i className="fa fa-bars hanmurger" />
+            </div>
+          </div>
+          <div className={`collapse ${isOpen ? 'open' : ''}`}>
+            <ul className="nav-items">
+              {data.map(({ title }, index) => (
+                <Menu
+                  key={index}
+                  title={title}
+                  index={index}
+                  onClick={() => {
+                    this.handleLinkClick();
+                  }}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-const Menu = ({ title, index }) => (
+const Menu = props => (
   <li className="nav-item">
-    {title === 'blog' ? (
+    {props.title === 'blog' ? (
       <a
         href="http://monolabs.io/blog"
         target="_blank"
         rel="noopener noreferrer"
       >
-        {title}
+        {props.title}
       </a>
     ) : (
       <Link
         activeClass="active"
-        to={`section${index}`}
+        to={`section${props.index}`}
         spy={true}
         smooth={true}
         offset={-70}
         duration={1000}
+        onClick={props.onClick}
       >
-        {title}
+        {props.title}
       </Link>
     )}
   </li>
